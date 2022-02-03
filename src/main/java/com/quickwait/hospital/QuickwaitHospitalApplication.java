87d1982e86c5a2ba74c175f0b2ea85527b661933
Daniel Dutra <danieldhsd@gmail.com>
2022-02-03 11:47:56 -0300

    Initial configuration

---
+++ src/main/java/com/quickwait/hospital/QuickwaitHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.quickwait.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickwaitHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickwaitHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickwait/hospital/QuickwaitHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickwait.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickwaitHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
